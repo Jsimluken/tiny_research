a28d69313947944b29d824fbbf9e49020df3e8f9
Michael Laurents <simonsluke134@gmail.com>
2020-01-07 22:47:01 +0900

    hyper tiny

---
+++ dataset.py
@@ -0,0 +1,129 @@
+def load_nsynth(path):
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+    dataset = tf.data.TFRecordDataset(path)
+    length = 64000
+    #dataset = dataset.apply(tf.data.experimental.shuffle_and_repeat(buffer_size=1000))
+    #dataset = dataset.apply(
+    #    tf.data.experimental.parallel_interleave(
+    #        tf.data.TFRecordDataset, cycle_length=20, sloppy=True))
+  
+    def _parse_nsynth(record):
+      
+      out = 8192
+      """Parsing function for NSynth dataset."""
+      features = {
+            'pitch': tf.io.FixedLenFeature([1], dtype=tf.int64),
+            'audio': tf.io.FixedLenFeature([length], dtype=tf.float32),
+            'qualities': tf.io.FixedLenFeature([10], dtype=tf.int64),
+            'instrument_str':tf.io.FixedLenFeature([1],dtype=tf.string),
+            'instrument_source': tf.io.FixedLenFeature([1], dtype=tf.int64),
+            'instrument_family': tf.io.FixedLenFeature([1], dtype=tf.int64),
+          }
+
+      example = tf.io.parse_single_example(record, features)
+      wave, pitch = example['audio'], example['pitch']
+      #instrument_num = tf.strings.to_number(tf.strings.split(example["instrument_str"],"_")[-1],out_type="int32")
+      instrument_str = tf.strings.to_number(tf.strings.split(example["instrument_str"],"_")[0][-1],out_type="int32")
+      qualities = example['qualities']
+      pitch = tf.cast(pitch,dtype="int32")
+      #pitch = tf.one_hot(pitch,depth=128)
+
+      #pitch = tf.keras.utils.to_categorical(pitch, num_classes=128)
+      family = tf.cast(example['instrument_family'],dtype="int32")
+      #family = tf.one_hot(family,depth=11)
+
+      inum = tf.reshape(instrument_str,shape=(1,))
+      return wave,pitch,family, inum,qualities
+    
+    def drop_quality(a,p,f,i,q):
+        return a,p,f,i
+
+
+    def mu_law(x, mu=255, int8=False):
+        """A TF implementation of Mu-Law encoding.
+            Args:
+            x: The audio samples to encode.
+            mu: The Mu to use in our Mu-Law.
+            int8: Use int8 encoding.
+            Returns:
+            out: The Mu-Law encoded int8 data.
+        """
+
+        x = x / (tf.reduce_max(x,axis=-1,keepdims=True))
+        x = x / (1+1e-5)
+        out = tf.sign(x) * tf.math.log(1 + mu * tf.abs(x)) / tf.math.log(1. + mu)
+        out = tf.floor(out * 128)
+        if int8:
+            out = tf.cast(out, tf.int8)
+        return out + 128.
+    def inv_mu_law(x, mu=255):
+      """A TF implementation of inverse Mu-Law.
+
+      Args:
+        x: The Mu-Law samples to decode.
+        mu: The Mu we used to encode these samples.
+
+      Returns:
+        out: The decoded data.
+      """
+      x = tf.cast(x, tf.float32) - 128.
+      out = (x + 0.5) * 2. / (mu + 1)
+      out = tf.sign(out) / mu * ((1 + mu)**tf.abs(out) - 1)
+      out = tf.where(tf.equal(x, 0), x, out)
+      return out
+
+    def mix(b,s):
+        return (b[0]+s[0])/2.,b,s
+
+    def to_categorical(a,p,f,i):
+        
+        
+        pitch = tf.one_hot(p[0],depth=128)
+        family = tf.one_hot(f[0],depth=11)
+        return a,pitch,family,i
+    
+    def random_crop(a,p,f,i):
+        start = tf.random.uniform(shape=(),minval=0,maxval= 4096,dtype=tf.int32)
+        audio = tf.slice(a,[start,],[8192,])
+        return audio,p,f,i
+    
+    def preprocess(w,l,f,i):
+        w = mu_law(w)
+        w = tf.one_hot(tf.cast(w,"int32"),256)
+        return w,l,f,i
+        
+    def preprocess(m,b,s):
+        #m_enc = tf.one_hot(tf.cast(mu_law(m),"int32"),256)
+        #b_enc = tf.one_hot(tf.cast(mu_law(b[0]),"int32"),256)
+        #s_enc = tf.one_hot(tf.cast(mu_law(s[0]),"int32"),256)
+        
+        m_enc = tf.expand_dims(tf.cast(mu_law(m),"float32"),axis=1)
+        b_enc = tf.expand_dims(tf.cast(mu_law(b[0]),"float32"),axis=1)
+        s_enc = tf.expand_dims(tf.cast(mu_law(s[0]),"float32"),axis=1)
+
+        return m_enc,(b_enc,)+b[1:],(s_enc,)+s[1:]
+
+
+    
+    
+    dataset = dataset.map(_parse_nsynth)
+
+    #dataset = dataset.filter(lambda w,l,i:tf.reduce_all(tf.equal(1,i)))
+    dataset = dataset.filter(lambda a,l,f,i,q:tf.equal(q[7],0)).map(drop_quality)
+    dataset1 = dataset.filter(lambda a,l,f,i,: tf.equal(f,0)[0]).cache("gs://hyper-tfrc_research/dataset/waveunet+pitch/nsynth-train-bass.cache").map(to_categorical)
+    dataset2 = dataset.filter(lambda a,l,f,i,: tf.equal(f,6)[0]).cache("gs://hyper-tfrc_research/dataset/waveunet+pitch/nsynth-train-organ.cache").map(to_categorical)
+    dataset1 = dataset1.shuffle(1000,reshuffle_each_iteration=True).repeat()
+    dataset2 = dataset2.shuffle(1000,reshuffle_each_iteration=True).repeat()
+    dataset1 = dataset1.map(random_crop).prefetch(AUTOTUNE)
+    dataset2 = dataset2.map(random_crop).prefetch(AUTOTUNE)
+    
+    bs_dataset= tf.data.Dataset.zip((dataset1,dataset2))
+    mixed_dataset = bs_dataset.map(mix)
+    mixed_dataset = mixed_dataset.map(preprocess)
+
+    #dataset = dataset.filter(lambda a,l,f,i:tf.math.logical_or(tf.equal(f,1)[0],tf.equal(f,8)[0])).cache("gs://hyper-ultimate-jsim-colab/dataset/nsynth-train-brass-string.cache")
+    #dataset = dataset.map(preprocess)
+    
+    
+    
+    return mixed_dataset
---
+++ model.py
@@ -0,0 +1,84 @@
+import os
+import tensorflow as tf
+from tensorflow.keras.layers import Conv1D, Input, BatchNormalization, Flatten, Dense,Conv2DTranspose,Reshape,Concatenate,Activation
+from tensorflow.keras.models import Model
+from enum import Enum
+import tqdm
+import datetime
+import numpy as np
+import time
+
+
+def create_encoder(initial_filter = 32):
+    input_ = Input((8192,1),name="audio")
+    x = input_
+    encs = []
+    num = 7
+    for i in range(num):
+        x = Conv1D(initial_filter*(i+1),15,strides=2,padding="same",activation="elu")(x)
+        x = BatchNormalization()(x)
+        encs.append(x)
+    return Model(input_,encs,name="encoder")
+
+def create_decoder(initial_filter = 32):
+  num = 7
+  inputs = [Input((64*2**i,initial_filter*(num-i)),name=f"enc{i}") for i in range(7)]
+  pitch = Input((128,),name="pitch")
+  instruments = Input((11,),name="instrument")
+
+  base = 64
+  enc = Reshape((1,64,initial_filter*7),name="renc0")(inputs[0])
+  infos = Concatenate(axis=-1)([pitch,instruments])
+  infos = Dense(64,activation="elu")(infos)
+  infos = Reshape((1,64,1))(infos)
+  enc = Concatenate(axis=-1)([enc,infos])
+  #dec = Conv2DTranspose(128,3,strides=(1,2))(enc)
+
+  for i in range(1,num):
+    dec = Conv2DTranspose(initial_filter*(num-i),7,strides=(1,2),padding="same",activation="relu")(enc)
+    dec = BatchNormalization()(dec)
+    enc = Reshape((1,64*2**i,initial_filter*(num-i)))(inputs[i])
+    enc = Concatenate(axis=-1)([dec,enc])
+  out = Conv2DTranspose(1,7,strides=(1,2),padding="same",activation="relu")(enc)
+  out = BatchNormalization()(out)
+  out = Reshape((8192,1),name="out_reshape")(out)
+  #out = Activation("softmax",name="out")(out)
+  out = Conv1D(1,7,padding="same",activation="relu")(out)
+
+  
+  return Model(inputs=[inputs,pitch,instruments],outputs=out,name="decoder")
+
+
+def create_classifier(initial_filter = 32):
+    input_ = Input((64,initial_filter*7),name="enc_input")
+    instruments_input = Input((11,))
+    instruments_i = Dense(64,activation="elu")(instruments_input)
+    instruments_i = Reshape((64,1))(instruments_i)
+    
+    #pitch = Conv1D(128,5,strides=2,activation="elu")(input_)
+
+
+    instruments = Conv1D(64,5,strides=2,activation="elu")(input_)
+    instruments = Conv1D(32,5,strides=2,activation="elu")(instruments)
+    instruments = Flatten()(instruments)
+    instruments = Dense(11, activation="softmax")(instruments)
+    
+    x = Concatenate(axis=-1)([input_,instruments_i])
+    pitch = Conv1D(64,5,strides=2,activation="elu")(x)
+    pitch = BatchNormalization()(pitch)
+    pitch = Conv1D(32,5,strides=2,activation="elu")(pitch)
+    pitch = BatchNormalization()(pitch)
+    pitch = Flatten()(pitch)
+    pitch = Dense(128, activation="softmax")(pitch)
+    
+    fake = Conv1D(64,5,strides=2,activation="elu")(x)
+    fake = Conv1D(32,5,strides=2,activation="elu")(fake)
+    fake = Flatten()(fake)
+    fake = Dense(1,"sigmoid")(fake)
+
+    #x = Flatten()(input_)
+    #x = Concatenate(axis=-1)([x,instruments_input])
+    #pitch = Dense(128, activation="softmax")(x)
+    #
+    #fake = Dense(1,activation="sigmoid")(x)
+    return Model([input_,instruments_input],[pitch,instruments,fake])
